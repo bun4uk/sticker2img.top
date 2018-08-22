4651d4b6c75b1d14510bd90c9f6ede97aa39a05a
V.Bunchuk <v.bunchuk@iotechnologies.com>
2018-08-22 17:15:34 +0300

    update

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -53,7 +53,7 @@ class DefaultController extends AbstractController
                 $user->setLastname($update->message->chat->last_name ?? null);
                 $user->setFirstLaunch(new \DateTime());
 
-                $telegramApi->sendMessage(7699150, 'New user ' . $user->getFirstname() . ' ' . $user->getLastname() . ' @' . $user->getUsername() . ' раз');
+                $telegramApi->sendMessage(7699150, 'New user ' . $user->getFirstname() . ' ' . $user->getLastname() . ' @' . $user->getUsername());
             }
             $user->setLastAction(new \DateTime());
             $entityManager->persist($user);
