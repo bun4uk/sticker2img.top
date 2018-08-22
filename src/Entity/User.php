d160eb97502624e8225e5fad4d79959215039540
bun4uk <vladimir.bunchuk@icloud.com>
2018-08-22 13:14:02 +0300

    create User entity

---
+++ src/Entity/User.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $chatId;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $firstLaunch;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $lastAction;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getChatId(): ?int
+    {
+        return $this->chatId;
+    }
+
+    public function setChatId(int $chatId): self
+    {
+        $this->chatId = $chatId;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(?string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(?string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(?string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getFirstLaunch(): ?\DateTimeInterface
+    {
+        return $this->firstLaunch;
+    }
+
+    public function setFirstLaunch(\DateTimeInterface $firstLaunch): self
+    {
+        $this->firstLaunch = $firstLaunch;
+
+        return $this;
+    }
+
+    public function getLastAction(): ?\DateTimeInterface
+    {
+        return $this->lastAction;
+    }
+
+    public function setLastAction(\DateTimeInterface $lastAction): self
+    {
+        $this->lastAction = $lastAction;
+
+        return $this;
+    }
+}
