0697cf2b989af8d01231d827cfc0ca61ce19ab98
bun4uk <vladimir.bunchuk@icloud.com>
2018-08-22 13:31:02 +0300

    Action entity

---
+++ src/Entity/Action.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ActionRepository")
+ */
+class Action
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $chatId;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $setName;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $fileId;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $filePath;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $time;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getChatId(): ?int
+    {
+        return $this->chatId;
+    }
+
+    public function setChatId(?int $chatId): self
+    {
+        $this->chatId = $chatId;
+
+        return $this;
+    }
+
+    public function getSetName(): ?string
+    {
+        return $this->setName;
+    }
+
+    public function setSetName(?string $setName): self
+    {
+        $this->setName = $setName;
+
+        return $this;
+    }
+
+    public function getFileId(): ?string
+    {
+        return $this->fileId;
+    }
+
+    public function setFileId(?string $fileId): self
+    {
+        $this->fileId = $fileId;
+
+        return $this;
+    }
+
+    public function getFilePath(): ?string
+    {
+        return $this->filePath;
+    }
+
+    public function setFilePath(?string $filePath): self
+    {
+        $this->filePath = $filePath;
+
+        return $this;
+    }
+
+    public function getTime(): ?\DateTimeInterface
+    {
+        return $this->time;
+    }
+
+    public function setTime(\DateTimeInterface $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+}
