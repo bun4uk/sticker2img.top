8122ad07f53d1a5546333c581aff13c636fe90b0
V.Bunchuk <v.bunchuk@iotechnologies.com>
2018-08-27 18:46:36 +0300

    btn fix

--- src/Service/TelegramBot.php
+++ src/Service/TelegramBot.php
@@ -103,7 +103,7 @@ class TelegramBot
         $content = array(
             'chat_id' => $chatId,
             'reply_markup' => $encodedMarkup,
-            'text' => 'Calls Count'
+            'text' => ' '
         );
 
         try {
