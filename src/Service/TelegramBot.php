86d959e8aad76e92f9abae1020ff1055b0e772ee
V.Bunchuk <v.bunchuk@iotechnologies.com>
2018-08-27 18:45:17 +0300

    btn fix

--- src/Service/TelegramBot.php
+++ src/Service/TelegramBot.php
@@ -95,7 +95,7 @@ class TelegramBot
         $replyMarkup = [
             'keyboard' => [
                 [
-                    'Calls Count'
+                    'CallsCount'
                 ]
             ]
         ];
