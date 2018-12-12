930f9b5f70adab8d359179d43d4c4ea0d043e0c9
V.Bunchuk <v.bunchuk@iotechnologies.com>
2018-12-12 19:48:19 +0200

    fix

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -81,7 +81,7 @@ class DefaultController extends AbstractController
                     $imgPathWebp
                 );
                 $telegramApi->sendPhoto($update->message->chat->id, $imgPathWebp);
-                $telegramApi->sendMessage($update->message->chat->id, $update->message->sticker);
+                $telegramApi->sendMessage($update->message->chat->id, json_encode($update->message->sticker));
                 unlink($imgPathWebp);
 
                 $action = new Action();
